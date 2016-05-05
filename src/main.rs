0b50c8a9e735ff2e2e6071d2e5a752f1499d23d7
Marvin Löbel <loebel.marvin@gmail.com>
2016-05-05 17:16:42 +0200

    Ported to the cube example

--- src/main.rs
+++ src/main.rs
@@ -17,34 +17,56 @@ extern crate gfx;
 extern crate gfx_window_glutin;
 extern crate gfx_device_gl;
 extern crate glutin;
+extern crate cgmath;
 
 use gfx::traits::FactoryExt;
+use gfx::Factory;
 use gfx::Device;
 
 pub type ColorFormat = gfx::format::Rgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
 
+// Declare the vertex format suitable for drawing,
+// as well as the constants used by the shaders
+// and the pipeline state object format.
+// Notice the use of FixedPoint.
 gfx_defines!{
     vertex Vertex {
-        pos: [f32; 2] = "a_Pos",
-        color: [f32; 3] = "a_Color",
+        pos: [i8; 4] = "a_Pos",
+        tex_coord: [i8; 2] = "a_TexCoord",
+    }
+
+    constant Locals {
+        transform: [[f32; 4]; 4] = "u_Transform",
     }
 
     pipeline pipe {
         vbuf: gfx::VertexBuffer<Vertex> = (),
-        out: gfx::RenderTarget<ColorFormat> = "Target0",
+        transform: gfx::Global<[[f32; 4]; 4]> = "u_Transform",
+        locals: gfx::ConstantBuffer<Locals> = "Locals",
+        color: gfx::TextureSampler<[f32; 4]> = "t_Color",
+        out_color: gfx::RenderTarget<ColorFormat> = "Target0",
+        out_depth: gfx::DepthTarget<DepthFormat> =
+            gfx::preset::depth::LESS_EQUAL_WRITE,
     }
 }
 
-const TRIANGLE: [Vertex; 3] = [
-    Vertex { pos: [ -0.5, -0.5 ], color: [1.0, 0.0, 0.0] },
-    Vertex { pos: [  0.5, -0.5 ], color: [0.0, 1.0, 0.0] },
-    Vertex { pos: [  0.0,  0.5 ], color: [0.0, 0.0, 1.0] }
-];
+impl Vertex {
+    fn new(p: [i8; 3], t: [i8; 2]) -> Vertex {
+        Vertex {
+            pos: [p[0], p[1], p[2], 1],
+            tex_coord: t,
+        }
+    }
+}
 
 const CLEAR_COLOR: [f32; 4] = [0.1, 0.2, 0.3, 1.0];
 
 pub fn main() {
+    use cgmath::{Point3, Vector3};
+    use cgmath::{Transform, AffineMatrix3};
+    use gfx::traits::FactoryExt;
+
     // Window builder
     let builder = glutin::WindowBuilder::new()
         .with_title("Triangle example [windowed]".to_string())
@@ -60,18 +82,81 @@ pub fn main() {
 
     // shader pipeline
     let pso = factory.create_pipeline_simple(
-        include_bytes!("shader/triangle_150.glslv"),
-        include_bytes!("shader/triangle_150.glslf"),
+        include_bytes!("shader/cube_150.glslv"),
+        include_bytes!("shader/cube_150.glslf"),
         pipe::new()
     ).unwrap();
 
+    let vertex_data = [
+        // top (0, 0, 1)
+        Vertex::new([-1, -1,  1], [0, 0]),
+        Vertex::new([ 1, -1,  1], [1, 0]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        Vertex::new([-1,  1,  1], [0, 1]),
+        // bottom (0, 0, -1)
+        Vertex::new([-1,  1, -1], [1, 0]),
+        Vertex::new([ 1,  1, -1], [0, 0]),
+        Vertex::new([ 1, -1, -1], [0, 1]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        // right (1, 0, 0)
+        Vertex::new([ 1, -1, -1], [0, 0]),
+        Vertex::new([ 1,  1, -1], [1, 0]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        Vertex::new([ 1, -1,  1], [0, 1]),
+        // left (-1, 0, 0)
+        Vertex::new([-1, -1,  1], [1, 0]),
+        Vertex::new([-1,  1,  1], [0, 0]),
+        Vertex::new([-1,  1, -1], [0, 1]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        // front (0, 1, 0)
+        Vertex::new([ 1,  1, -1], [1, 0]),
+        Vertex::new([-1,  1, -1], [0, 0]),
+        Vertex::new([-1,  1,  1], [0, 1]),
+        Vertex::new([ 1,  1,  1], [1, 1]),
+        // back (0, -1, 0)
+        Vertex::new([ 1, -1,  1], [0, 0]),
+        Vertex::new([-1, -1,  1], [1, 0]),
+        Vertex::new([-1, -1, -1], [1, 1]),
+        Vertex::new([ 1, -1, -1], [0, 1]),
+    ];
+
+    let index_data: &[u16] = &[
+        0,  1,  2,  2,  3,  0, // top
+        4,  5,  6,  6,  7,  4, // bottom
+        8,  9, 10, 10, 11,  8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // front
+        20, 21, 22, 22, 23, 20, // back
+    ];
+
     // vertex buffer
-    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice(&TRIANGLE, ());
+    let (vbuf, slice) = factory.create_vertex_buffer_with_slice(&vertex_data, index_data);
+
+    let texels = [[0x20, 0xA0, 0xC0, 0x00]];
+    let (_, texture_view) = factory.create_texture_const::<gfx::format::Rgba8>(
+        gfx::tex::Kind::D2(1, 1, gfx::tex::AaMode::Single), &[&texels]
+        ).unwrap();
+
+    let sinfo = gfx::tex::SamplerInfo::new(
+        gfx::tex::FilterMethod::Bilinear,
+        gfx::tex::WrapMode::Clamp);
+
+
+    let view: AffineMatrix3<f32> = Transform::look_at(
+        Point3::new(1.5f32, -5.0, 3.0),
+        Point3::new(0f32, 0.0, 0.0),
+        Vector3::unit_z(),
+    );
+    let proj = cgmath::perspective(cgmath::deg(45.0f32), 4.0/3.0, 1.0, 10.0);
 
     // data pipeline
     let data = pipe::Data {
-        vbuf: vertex_buffer,
-        out: main_color
+        vbuf: vbuf,
+        transform: (proj * view.mat).into(),
+        locals: factory.create_constant_buffer(1),
+        color: (texture_view, factory.create_sampler(sinfo)),
+        out_color: main_color,
+        out_depth: _main_depth,
     };
 
     // main loop
@@ -89,7 +174,10 @@ pub fn main() {
             }
         }
         // draw a frame
-        encoder.clear(&data.out, CLEAR_COLOR);
+        let locals = Locals { transform: data.transform };
+        encoder.update_constant_buffer(&data.locals, &locals);
+        encoder.clear(&data.out_color, CLEAR_COLOR);
+        encoder.clear_depth(&data.out_depth, 1.0);
         encoder.draw(&slice, &pso, &data);
         encoder.flush(&mut device);
         window.swap_buffers().unwrap();
